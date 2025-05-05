ac802baa4f1e428681bca24cf4095b7defc09d55
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2025-05-05 14:44:43 -0400

    Started work on paginating results

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,18 @@ import "./SearchStyle.css";
 import { useEffect, useState } from "react";
 
 
+function navigatePages(url) {
+
+}
+
 function Search() {
 
     //State for displaying the breed options
     const [breedOptions, setBreedOptions] = useState([]);
 
     const [resultsData, setResultsData] = useState();
+
+    const [dogDetails, setDogDetails] = useState([]);
 
     const [nextUrl, setNextUrl] = useState("");
     const [prevUrl, setPrevUrl] = useState("");
@@ -72,7 +78,7 @@ function Search() {
         
     }
 
-    //Hook that paginates results
+    //Hook that paginates results and sets the url for the next and previous pages
     useEffect(() => {
         
         console.log("Results outside statements: ", resultsData);
@@ -81,18 +87,42 @@ function Search() {
             console.log("URL for next page: ", resultsData.next);
             setNextUrl(`https://frontend-take-home-service.fetch.com` + resultsData.next);
             
-            //Fix this by using an useEffect hook
-            console.log("NextURL value: ", nextUrl);
+            if("prev" in resultsData) {
+                setPrevUrl(`https://frontend-take-home-service.fetch.com` + resultsData.prev);
+            }
+            
+            let dogIds = JSON.stringify(resultsData.resultIds);
+
+            fetch(`https://frontend-take-home-service.fetch.com/dogs/`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: dogIds
+    
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                setDogDetails(data);
+            })
+            .catch((error) => console.error("Error fetching dogs", error));
+
+            // let resultsArr = [];
+            // resultsArr = searchDogs(resultsData.resultIds);
 
         }
     
     }, [resultsData]);
 
+    console.log("Decoded Ids: ", dogDetails);
+    console.log("NextURL value: ", nextUrl);
+    console.log("PrevURL value: ", prevUrl);
+
     return (
         <div id="search_bg">
             <form id="search_form" onSubmit={handleSearch}>
                 <label id="breed_label" for="breed">Breed: </label>
-                {/* <select name="Breed" id="breed" value={selectedBreed} onChange={handleBreedChange}> */}
                 <select name="Breed" id="breed">    
                     <option value="">Select a breed</option>
                     {breedOptions.map((breed, index) => {
@@ -101,6 +131,26 @@ function Search() {
                 </select>
                 <button id="submit_btn" type="submit">Search</button>
             </form>
+            
+            <div id="display_dogs">
+                <ol id="dog_list" type="A">
+                    {dogDetails.map((dog, index) => {
+                        return <li id="dog_il" key={index}>
+                            <div>
+                                <p>{dog["name"]}</p>
+                                {/* <p>{dog["img"]}</p>
+                                <p>{dog["age"]}</p>
+                                <p>{dog["breed"]}</p>
+                                <p>{dog["zip_code"]}</p> */}
+
+                            </div>
+                        </li>
+                    })}
+                </ol>
+            </div>
+
+            <button id="next_btn" onClick={navigatePages(nextUrl)}>Next</button>
+            <button id="prev_btn" onClick={navigatePages(prevUrl)}>Previous</button>
         </div>
     )
 
